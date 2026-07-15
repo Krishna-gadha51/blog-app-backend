88730698454c57376ebb9fad36334ff9764542fb
Krishnagadha <krishnagadhaurakkat@gmail.com>
2026-07-15 21:53:00 +0530

    signup api completed

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 const Express = require('express');
-const Mongoose = require('mongoose');
+ const Mongoose = require('mongoose');
 const Bcrypt = require('bcrypt');
-const Jwt = require('jsonwebtoken');
+// const Jwt = require('jsonwebtoken');
 const Cors = require('cors');
+const userModel = require('./models/users.js');
+
 
 
 let app = Express();
 
+app.use(Express.json());
+app.use(Cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello ');
-})
+
+
+
+Mongoose.connect("mongodb://krishnagadha:gadha55@ac-c6tjfyj-shard-00-00.7plurii.mongodb.net:27017,ac-c6tjfyj-shard-00-01.7plurii.mongodb.net:27017,ac-c6tjfyj-shard-00-02.7plurii.mongodb.net:27017/blogappdb?ssl=true&replicaSet=atlas-vpubjr-shard-0&authSource=admin&appName=Cluster0")
+
+app.post('/signup',async (req, res) => {
+    let input = req.body;
+    let hashedPassword = Bcrypt.hashSync(req.body.password, 10);
+    console.log(hashedPassword);
+    req.body.password = hashedPassword;
+    
+    
+     userModel.find({ email: req.body.email }).then(
+        (items)=>{
+       
+    
+        if (items.length > 0) {
+
+            res.json({ "status": "email id already exists" });
+         } 
+         else {
+             let result=new userModel(input);
+              result.save();
+             res.json({ "status": "created successfully" });
+       
+    }
+    });
+});
 
 
 app.listen (3030, () => {
